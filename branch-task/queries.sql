bf741c9bf0b1de8f7b7751c90d9fb810882888d7
Nikita_Larionov11 Nikita_Larionov11 <nikitalar005@gmail.com>
2024-03-22 14:24:24 +0300

    Упражняюсь с SQL + ветки

---
+++ branch-task/queries.sql
@@ -0,0 +1,23 @@
+create table employee
+(
+    id         bigserial,
+    fio        varchar(100),
+    manager_id int,
+    salary     int
+);
+insert into employee (fio, salary)
+values ('Вася', 300);
+
+insert into employee (fio, manager_id, salary)
+values ('Дима', (select id from employee where fio = 'Вася'), 280);
+
+insert into employee (fio, manager_id, salary)
+values ('Оля', (select id from employee where fio = 'Вася'), 270);
+
+insert into employee (fio, salary)
+values ('Коля', 500);
+
+select *
+from employee
+where manager_id IS NULL
+order by salary desc;
